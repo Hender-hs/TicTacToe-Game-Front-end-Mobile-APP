77988ec322cecfa86ed31305bc878b156a10357d
hender-hs <henderson.f.p@hotmail.com>
2022-06-08 20:16:43 -0300

    feat: sqaure's component styles

---
+++ src/styles/components/squares/squares.styles.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+	"square": {
+    	"width": '33%',
+    	"height": '33%',
+    	"borderWidth": 5,
+    	"borderStyle": 'solid',
+    	"borderColor": '#000000',
+    	"backgroundColor": "white"
+  	}
+})
